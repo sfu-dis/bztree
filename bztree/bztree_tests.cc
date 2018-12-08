4c51baa4f3f0c455d01da512f713a0f17597a114
Tianzheng Wang <tzwang@sfu.ca>
2018-12-07 23:41:23 -0800

    Add dummy bztree test.

---
+++ bztree/bztree_tests.cc
@@ -0,0 +1,12 @@
+#include <glog/logging.h>
+#include <gtest/gtest.h>
+
+#include "bztree.h"
+
+TEST(LeafNode, Insert) {
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
