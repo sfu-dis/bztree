79a9cd3b1ca0940ecef233b7168dc0aeea76b4d3
Tianzheng Wang <tzwang@sfu.ca>
2018-12-09 16:12:34 -0800

    Add a simple test for leaf node insert.

--- bztree/bztree_tests.cc
+++ bztree/bztree_tests.cc
@@ -4,6 +4,27 @@
 #include "bztree.h"
 
 TEST(LeafNode, Insert) {
+  bztree::LeafNode *node = (bztree::LeafNode*)malloc(bztree::LeafNode::kNodeSize);
+  memset(node, 0, bztree::LeafNode::kNodeSize);
+  new (node) bztree::LeafNode;
+  node->Dump();
+
+  pmwcas::InitLibrary(pmwcas::TlsAllocator::Create,
+                      pmwcas::TlsAllocator::Destroy,
+                      pmwcas::LinuxEnvironment::Create,
+                      pmwcas::LinuxEnvironment::Destroy);
+
+  pmwcas::DescriptorPool *pool =
+    (pmwcas::DescriptorPool*)pmwcas::Allocator::Get()->Allocate(sizeof(pmwcas:: DescriptorPool));
+  new (pool) pmwcas::DescriptorPool(1000, 1, nullptr, false);
+
+  pool->GetEpoch()->Protect();
+
+  ASSERT_TRUE(node->Insert(0, "abc", 3, 100, pool));
+  ASSERT_TRUE(node->Insert(0, "def", 3, 100, pool));
+
+  pool->GetEpoch()->Unprotect();
+  node->Dump();
 }
 
 int main(int argc, char **argv) {
