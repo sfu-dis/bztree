157e3a9ec03b984a4cfc3f2c2d9516cde8c06a34
Tianzheng Wang <tzwang@sfu.ca>
2018-12-16 13:31:27 -0800

    No need to cast to char* with const char* Insert interface.

--- bztree/bztree_tests.cc
+++ bztree/bztree_tests.cc
@@ -65,9 +65,9 @@ TEST_F(LeafNodeFixtures, Read) {
 TEST_F(LeafNodeFixtures, Insert) {
   pool->GetEpoch()->Protect();
 
-  ASSERT_TRUE(node->Insert(0, (char *)"def", 3, 100, pool));
-  ASSERT_TRUE(node->Insert(0, (char *)"bdef", 4, 101, pool));
-  ASSERT_TRUE(node->Insert(0, (char *)"abc", 3, 102, pool));
+  ASSERT_TRUE(node->Insert(0, "def", 3, 100, pool));
+  ASSERT_TRUE(node->Insert(0, "bdef", 4, 101, pool));
+  ASSERT_TRUE(node->Insert(0, "abc", 3, 102, pool));
   ASSERT_EQ(node->Read("def", 3), 100);
   ASSERT_EQ(node->Read("abc", 3), 102);
 
@@ -122,13 +122,13 @@ TEST_F(LeafNodeFixtures, SplitPrep) {
   pool->GetEpoch()->Protect();
   InsertDummy();
 
-  ASSERT_TRUE(node->Insert(0, (char *) "abc", 3, 100, pool));
-  ASSERT_TRUE(node->Insert(0, (char *) "bdef", 4, 101, pool));
-  ASSERT_TRUE(node->Insert(0, (char *) "abcd", 4, 102, pool));
-  ASSERT_TRUE(node->Insert(0, (char *) "deadbeef", 8, 103, pool));
-  ASSERT_TRUE(node->Insert(0, (char *) "parker", 6, 104, pool));
-  ASSERT_TRUE(node->Insert(0, (char *) "deadpork", 8, 105, pool));
-  ASSERT_TRUE(node->Insert(0, (char *) "toronto", 7, 106, pool));
+  ASSERT_TRUE(node->Insert(0, "abc", 3, 100, pool));
+  ASSERT_TRUE(node->Insert(0, "bdef", 4, 101, pool));
+  ASSERT_TRUE(node->Insert(0, "abcd", 4, 102, pool));
+  ASSERT_TRUE(node->Insert(0, "deadbeef", 8, 103, pool));
+  ASSERT_TRUE(node->Insert(0, "parker", 6, 104, pool));
+  ASSERT_TRUE(node->Insert(0, "deadpork", 8, 105, pool));
+  ASSERT_TRUE(node->Insert(0, "toronto", 7, 106, pool));
 
   node->Dump();
 
