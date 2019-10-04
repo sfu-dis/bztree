7e1d7204ac1dd792371905d273165576375727a4
Xiangpeng Hao <xha62@sfu.ca>
2019-10-04 13:21:33 -0700

    add bztree pibench wrapper

---
+++ tests/bztree_pibench_wrapper.cc
@@ -0,0 +1,104 @@
+#include "bztree_pibench_wrapper.h"
+
+extern "C" tree_api* create_tree(const tree_options_t &opt)
+{
+    return new bztree_wrapper(opt);
+}
+
+bztree_wrapper::bztree_wrapper(const tree_options_t &opt) {
+    // FIXME(tzwang): vary the parameters later; here just for demo purpose
+    bztree::BzTree::ParameterSet param(1024, 512, 1024);
+
+#ifdef PMDK
+    pmwcas::InitLibrary(pmwcas::PMDKAllocator::Create(opt.pool_path.c_str(), "bztree_layout", opt.pool_size),
+                        pmwcas::PMDKAllocator::Destroy,
+                        pmwcas::LinuxEnvironment::Create,
+                        pmwcas::LinuxEnvironment::Destroy);
+    auto pmdk_allocator = reinterpret_cast<pmwcas::PMDKAllocator *>(pmwcas::Allocator::Get());
+    bztree::Allocator::Init(pmdk_allocator);
+
+    auto *bztree = reinterpret_cast<bztree::BzTree *>(pmdk_allocator->GetRoot(sizeof(bztree::BzTree)));
+    pmwcas::DescriptorPool *pool = nullptr;
+    pmdk_allocator->Allocate((void **)&pool, sizeof(pmwcas::DescriptorPool));
+    new(pool) pmwcas::DescriptorPool(100000, opt.num_threads, false);
+
+    new(bztree)bztree::BzTree(param, pool, reinterpret_cast<uint64_t>(pmdk_allocator->GetPool()));
+    pmdk_allocator->PersistPtr(bztree, sizeof(bztree::BzTree));
+    pmdk_allocator->PersistPtr(pool, sizeof(pmwcas::DescriptorPool));
+#else
+    // Volatile variant
+    //pmwcas::InitLibrary(pmwcas::DefaultAllocator::Create,
+    //                    pmwcas::DefaultAllocator::Destroy,
+    pmwcas::InitLibrary(pmwcas::TlsAllocator::Create,
+                        pmwcas::TlsAllocator::Destroy,
+                        pmwcas::LinuxEnvironment::Create,
+                        pmwcas::LinuxEnvironment::Destroy);
+    auto pool = new pmwcas::DescriptorPool(100000, opt.num_threads, false);
+    auto *bztree = bztree::BzTree::New(param, pool);
+#endif
+
+    tree_ = bztree;
+}
+
+bztree_wrapper::~bztree_wrapper() {
+    pmwcas::Thread::ClearRegistry();
+}
+
+bool bztree_wrapper::find(const char* key, size_t key_sz, char *value_out) {
+    // FIXME(tzwang): for now only support 8-byte values
+    uint64_t k = __builtin_bswap64(*reinterpret_cast<const uint64_t*>(key));
+    return tree_->Read(reinterpret_cast<const char*>(&k), key_sz, (uint64_t *)value_out).IsOk();
+}
+
+bool bztree_wrapper::insert(const char* key, size_t key_sz, const char* value, size_t value_sz) {
+    // FIXME(tzwang): for now only support 8-byte values
+    assert(value_sz == sizeof(uint64_t));
+    uint64_t k = __builtin_bswap64(*reinterpret_cast<const uint64_t*>(key));
+    uint64_t v = *reinterpret_cast<uint64_t*>(const_cast<char*>(value));
+
+    // Mask out the 3 MSBs
+    v &= 0x1FFFFFFFFFFFFFFF;
+    return tree_->Insert(reinterpret_cast<const char*>(&k), key_sz, v).IsOk();
+}
+
+bool bztree_wrapper::update(const char* key, size_t key_sz, const char* value, size_t value_sz) {
+    // FIXME(tzwang): for now only support 8-byte values
+    assert(value_sz == sizeof(uint64_t));
+    uint64_t k = __builtin_bswap64(*reinterpret_cast<const uint64_t*>(key));
+    uint64_t v = *reinterpret_cast<uint64_t*>(const_cast<char*>(value));
+
+    // Mask out the 3 MSBs
+    v &= 0x1FFFFFFFFFFFFFFF;
+    return tree_->Update(reinterpret_cast<const char*>(&k), key_sz, v).IsOk();
+}
+
+bool bztree_wrapper::remove(const char* key, size_t key_sz) {
+    uint64_t k = __builtin_bswap64(*reinterpret_cast<const uint64_t*>(key));
+    return tree_->Delete(reinterpret_cast<const char*>(&k), key_sz).IsOk();
+}
+
+int bztree_wrapper::scan(const char* key, size_t key_sz, int scan_sz, char*& values_out) {
+    static thread_local std::array<char,(1<<20)> results;
+    uint64_t k = __builtin_bswap64(*reinterpret_cast<const uint64_t*>(key));
+
+    int scanned = 0;
+    char* dst = results.data();
+
+    auto iter = tree_->RangeScanBySize(reinterpret_cast<const char*>(&k), key_sz, scan_sz);
+    for(scanned=0; (scanned < scan_sz); ++scanned)
+    {
+        auto record = iter->GetNext();
+        if(record == nullptr)
+            break;
+
+        uint64_t result_key = __builtin_bswap64(*reinterpret_cast<const uint64_t*>(record->GetKey()));
+        memcpy(dst, &result_key, sizeof(uint64_t));
+        dst += sizeof(uint64_t);
+
+        auto payload = record->GetPayload();
+        memcpy(dst, &payload, sizeof(uint64_t));
+        dst += sizeof(uint64_t);
+    }
+    values_out = results.data();
+    return scanned;
+}
---
+++ tests/bztree_pibench_wrapper.h
@@ -0,0 +1,21 @@
+#pragma once
+// A wrapper for the Bz-Tree
+
+#include "tree_api.hpp"
+#include "../bztree.h"
+
+class bztree_wrapper : public tree_api
+{
+public:
+    bztree_wrapper(const tree_options_t &opt);
+    virtual ~bztree_wrapper();
+
+    virtual bool find(const char* key, size_t key_sz, char *value_out) override;
+    virtual bool insert(const char* key, size_t key_sz, const char* value, size_t value_sz) override;
+    virtual bool update(const char* key, size_t key_sz, const char* value, size_t value_sz) override;
+    virtual bool remove(const char* key, size_t key_sz) override;
+    virtual int scan(const char* key, size_t key_sz, int scan_sz, char*& values_out) override;
+
+private:
+    bztree::BzTree *tree_;
+};
