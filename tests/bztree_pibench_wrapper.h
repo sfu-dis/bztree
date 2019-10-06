82de6b1a59d395453b574ecfae78f7287cbf4af9
Xiangpeng Hao <xha62@sfu.ca>
2019-10-06 14:38:18 -0700

    add recovery wrapper

--- tests/bztree_pibench_wrapper.h
+++ tests/bztree_pibench_wrapper.h
@@ -10,11 +10,13 @@ public:
     bztree_wrapper(const tree_options_t &opt);
     virtual ~bztree_wrapper();
 
-    virtual bool find(const char* key, size_t key_sz, char *value_out) override;
-    virtual bool insert(const char* key, size_t key_sz, const char* value, size_t value_sz) override;
-    virtual bool update(const char* key, size_t key_sz, const char* value, size_t value_sz) override;
-    virtual bool remove(const char* key, size_t key_sz) override;
-    virtual int scan(const char* key, size_t key_sz, int scan_sz, char*& values_out) override;
+    virtual bool find(const char *key, size_t key_sz, char *value_out) override;
+    virtual bool insert(const char *key, size_t key_sz, const char *value, size_t value_sz) override;
+    virtual bool update(const char *key, size_t key_sz, const char *value, size_t value_sz) override;
+    virtual bool remove(const char *key, size_t key_sz) override;
+    virtual int scan(const char *key, size_t key_sz, int scan_sz, char *&values_out) override;
+
+    bool recovery(const tree_options_t &opt);
 
 private:
     bztree::BzTree *tree_;
